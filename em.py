ff41771d7a1148ff14ec6622efffa70cf4873434
Yosuke Higuchi <wasapon.dev@gmail.com>
2018-01-27 15:26:53 +0900

    first commit

---
+++ em.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def gaussian(mu, sig):
+    def f(x):
+        
+
+m = np.array([1 / 3, 1 / 3, 1 / 3])
+mu = np.array([7.0, 3.0, 5.0])
+sig = np.array([1.0, 1.0, 1.0])
+
+
+for i in range(0, 3):
